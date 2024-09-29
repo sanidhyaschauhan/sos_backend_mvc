15cb52f0a5e461774237ec149689af4ebeac1a28
sanidhyaschauhan <155995689+sanidhyaschauhan@users.noreply.github.com>
2024-09-28 21:05:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from models.emergency_model import EmergencyModel
+from neurelo_client import NeureloClient
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -19,9 +20,19 @@ def analyze():
 
     return jsonify(result)
 
+@app.route('/getalldata', methods=['GET'])
+def get_all_reports():
+    try:
+        neurelo_client = NeureloClient()
+        all_reports = neurelo_client.get_all_reports()
+        return jsonify(all_reports)
+    except Exception as e:
+        return {"error": f"An unexpected error occurred: {e}"}
+
 @app.route('/server_status', methods=['GET'])
 def server_status():
     return "Server is up"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
